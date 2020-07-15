a94ab17d2edbd2d5f5f8141e3522f424db43bdc9
phamthanhtuan100mc <phamthanhtuan100mc@gmail.com>
2020-07-15 23:43:58 +0700

    merge code face detect vs this app

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -2,19 +2,15 @@ import React, { useState, useEffect, useRef } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
   TouchableOpacity,
-  StyleSheet,
   StatusBar,
   Alert,
   Image,
-  ActivityIndicator
 } from 'react-native';
 import { Camera } from 'expo-camera';
 import { BlurView } from 'expo-blur';
 import {
   Provider as PaperProvider,
-  Button,
   ActivityIndicator,
   Colors,
   Button,
@@ -23,7 +19,7 @@ import {
 import MaterialIcons from 'react-native-vector-icons/FontAwesome';
 import * as Location from 'expo-location';
 import { getPreciseDistance, isPointWithinRadius } from 'geolib';
-import Config from './config.json'
+import Config from '../config.json'
 
 const DetailScreen = ({ navitgation }) => {
 
@@ -31,15 +27,17 @@ const DetailScreen = ({ navitgation }) => {
   const [myLocation, setMyLocation] = useState(null);
   const [checkinStatus, setCheckinStatus] = useState(false);
   const [withinClass, setWithinClass] = useState(false);
-  const [radius, setRadius] = useState(50);
+  const [radius, setRadius] = useState(10);
   const toggleSwitch = () => setCheckinStatus(previousState => !previousState);
   // const [checkinStatus, setCheckinStatus] = useState();
   // const [isEnabled, setIsEnabled] = useState(false);
 
   // Tuan's home
   // 10.802919, 106.715229
+  // 10.802795, 106.715138
+  // fail location 10.803674, 106.688265
   // temp data, waiting for teacher's app setting
-  const [teacherLocation, setTeacherLocation] = useState({ latitude: 10.7710649, longitude: 106.7032397 });
+  const [teacherLocation, setTeacherLocation] = useState({ latitude: 10.802795, longitude: 106.715138 });
 
   const cameraRef = useRef()
   const [hasPermission, setHasPermission] = useState(null)
@@ -59,13 +57,15 @@ const DetailScreen = ({ navitgation }) => {
   useEffect(() => {
 
     (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
+      let { locationStatus } = await Location.requestPermissionsAsync();
+      if (locationStatus !== 'granted') {
         setErrorMsg('Quyền truy cập vị trí đã bị từ chối');
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      console.log("DetailScreen -> location", location)
+
+      const { cameraStatus } = await Camera.requestPermissionsAsync();
+      setHasPermission(cameraStatus === 'granted');
 
       setMyLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude });
       setWithinClass(isPointWithinRadius(
@@ -73,16 +73,9 @@ const DetailScreen = ({ navitgation }) => {
         teacherLocation,
         radius
       ))
+
+
     })();
-
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-
-    // return (
-
-    // )
 
   });
 
@@ -480,68 +473,51 @@ const DetailScreen = ({ navitgation }) => {
 
   return (
     <PaperProvider>
+      <View style={styles.topContainer}></View>
       <View style={styles.container}>
         {
-          myLocation ?
-            // myLocation = true
-            // Đã có location, tiến hình kiểm tra trong phạm vi lớp hay không
-            <View style={styles.container}>
-              {
-                withinClass ?
-                  <View style={styles.container}>
-                    {/* danger here */}
-                    {
-                      isShot ?
-                        <View
-                          style={styles.cmrContainer}>
-                          <Image style={styles.blurredImage} source={{ faceUri }} />
-                          <BlurView intensity={250} style={[StyleSheet.absoluteFill, styles.nonBlurredContent]}>
-                          </BlurView>
-                        </View>
-                        :
-                        <Camera
-                          ref={cameraRef}
-                          type={Camera.Constants.Type.front}
-                          ratio='4:3'
-                          style={styles.cmrContainer}>
-                        </Camera>
-                    }
+          myLocation && withinClass && !isShot &&
+          <>
+            <Camera
+              ref={cameraRef}
+              type={Camera.Constants.Type.front}
+              ratio='4:3'
+              style={styles.cmrContainer}>
+            </Camera>
 
-                    <View style={styles.camer_button_container}>
+            <TouchableOpacity onPress={takePicture} style={styles.btnContainer}>
+              <MaterialIcons name="circle" size={80} color="#fff" />
+            </TouchableOpacity>
+          </>
+        }
+        {
+          myLocation && withinClass && isShot &&
+          <>
+            <Image style={styles.blurredImage} source={{ faceUri }} />
+            <BlurView intensity={250} style={[StyleSheet.absoluteFill, styles.cmrContainer]}>
+            </BlurView>
 
-                      {
-                        isShot ?
-                          <>
-                            <ActivityIndicator
-                              animating={isShot}
-                              size="large"
-                              color={Colors.red800} />
-                            <Text style={styles.textStyle}>Đang quét khuôn mặt...</Text>
-                          </>
-                          :
-                          <TouchableOpacity onPress={takePicture} style={styles.capture}>
-                            <MaterialIcons name="circle" size={80} color="#fff" />
-                          </TouchableOpacity>
-                      }
-                    </View>
-                    {/* danger here */}
-                  </View>
-                  :
-                  <View>
-                    {/* withinClass = false */}
-                    {/* Báo ở ngoài lớp học, chưa cho điểm danh */}
-                    <Text>Bạn ở ngoài lớp học</Text>
-                    <Text>Không thể điểm danh</Text>
-                  </View>
-              }
-            </View>
-            :
-            <View>
-              {/* myLocation = false */}
-              {/* Chưa có location, tiếp tục xoay xoay xoay :))) */}
-              <Text>{text}</Text>
-              <ActivityIndicator size="large" color="#0000ff" />
-            </View>
+            <ActivityIndicator
+              animating={isShot}
+              size="large"
+              color={Colors.red800} />
+            <Text style={styles.textStyle}>Đang quét khuôn mặt...</Text>
+          </>
+        }
+        {
+          myLocation && !withinClass &&
+          <View>
+            <Text>Bạn ở ngoài lớp học</Text>
+            <Text>Không thể điểm danh</Text>
+          </View>
+        }
+        {
+          !myLocation &&
+          <View>
+            {/* Chưa có location */}
+            <Text>{text}</Text>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
         }
       </View>
     </PaperProvider>
@@ -549,53 +525,51 @@ const DetailScreen = ({ navitgation }) => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  containerTop: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  containerBottom: {
-    flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    flexDirection: 'row',
-    // backgroundColor: 'grey'    
-  },
-  subLeftContainer: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    flexDirection: 'column',
-    // paddingLeft: 20
-    // marginLeft: 20,
-  },
-  subRightContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    height: 50,
-    marginRight: 20,
-  },
-  text: {
-    fontSize: 17,
-    color: 'black',
 
+
+
+  topContainer: {
+    flex: 1 / 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    tintColor: "#fff"
+    // paddingTop: StatusBar.currentHeight,
   },
-  subText: {
-    // fontSize: 17,
-    color: 'grey',
+  container: {
+    flex: 9/10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#000'
   },
-  switchStyle: {
-    transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }],
+
+
+
+  textStyle: {
+    margin: 10,
+    color: '#fff'
   },
-  btnStyle: {
-    width: 440
-  }
+  btnContainer: {
+    flex: 3 / 10,
+    // flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000'
+  },
+  button: {
+    margin: 10
+  },
+  nonBlurredContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cmrContainer: {
+    width: "100%",
+    flex: 7 / 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+  },
 })
 
 export default DetailScreen
