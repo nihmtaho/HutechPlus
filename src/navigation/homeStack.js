67713addf8b6468c56fe19c90fdbc8a863e2b05b
thaotruogg <thaotruogg@gmail.com>
2020-08-01 23:33:19 +0700

    change color header

--- src/navigation/homeStack.js
+++ src/navigation/homeStack.js
@@ -1,38 +1,48 @@
-import React from 'react'
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import React from "react";
+import {
+	createStackNavigator,
+	TransitionPresets,
+} from "@react-navigation/stack";
 // import HomeScreen from '../../screens/HomeScreen';
-import HomeScreen from '../../screens/HomeScreenv2';
-import DetailScreen from '../../screens/DetailScreen';
+import HomeScreen from "../../screens/HomeScreenv2";
+import DetailScreen from "../../screens/DetailScreen";
 
 const HomeStack = createStackNavigator();
 
 const homeStack = ({ navigation, route }) => {
-  if (route.state && route.state.index > 0) {
-    navigation.setOptions({ tabBarVisible: false })
-  }
-  else {
-    navigation.setOptions({ tabBarVisible: true })
-  }
-  return (
-    <HomeStack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        gestureEnabled: true,
-        gestureDirection: 'horizontal',
-      }}>
-      <HomeStack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, }} />
-      <HomeStack.Screen name="Detail" component={DetailScreen} options={{
-        title: 'Điểm danh',
-        headerTransparent: true,
-        headerTitleStyle: {
-          color: '#fff',
-          fontWeight: "bold"
-        },
-        headerTitleAlign: "center",
-        ...TransitionPresets.SlideFromRightIOS
-      }} />
-    </HomeStack.Navigator>
-  )
-}
+	if (route.state && route.state.index > 0) {
+		navigation.setOptions({ tabBarVisible: false });
+	} else {
+		navigation.setOptions({ tabBarVisible: true });
+	}
+	return (
+		<HomeStack.Navigator
+			initialRouteName="Home"
+			screenOptions={{
+				gestureEnabled: true,
+				gestureDirection: "horizontal",
+			}}
+		>
+			<HomeStack.Screen
+				name="Home"
+				component={HomeScreen}
+				options={{ headerShown: false }}
+			/>
+			<HomeStack.Screen
+				name="Detail"
+				component={DetailScreen}
+				options={{
+					title: "Điểm danh",
+					headerStyle: {
+						backgroundColor: "#f4511e",
+					},
+					headerTintColor: "#fff",
+					headerTitleAlign: "center",
+					...TransitionPresets.SlideFromRightIOS,
+				}}
+			/>
+		</HomeStack.Navigator>
+	);
+};
 
 export default homeStack;
